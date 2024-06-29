28d3fad19df1275a8d5b61d613ef6145fce3eb6a
Ndoria <k.ndoriamurigu@gmail.com>
2024-06-29 03:12:57 +0300

    modifies objects

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Write your solution in this file!
+const employee = {
+    name: "Kihoto",
+    streetAddress: "Ongata Rongai",
+};
+
+function updateEmployeeWithKeyAndValue(obj, key, value) {
+    return {
+    ...obj,
+    [key]: value
+    }
+}
+oldEmployee = updateEmployeeWithKeyAndValue(employee, "hobby", "Skating")
+console.log(oldEmployee)
+
+
+newEmployee = updateEmployeeWithKeyAndValue(employee, "skinColor", "brown");
+console.log(newEmployee)
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value) {
+    obj[key] = value
+    return obj;
+}
+ragEmployee = destructivelyUpdateEmployeeWithKeyAndValue(employee, 'aura', "fantastic")
+console.log(ragEmployee)
+
+console.log(employee)
+
+function deleteFromEmployeeByKey(obj, key) {
+    delete obj.key;
+    return {
+        obj,
+    }
+}
+
+function destructivelyDeleteFromEmployeeByKey(obj, key) {
+    delete obj[key]
+    return obj
+}
+
